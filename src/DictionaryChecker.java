5c4b17fb6b3bbfa5ecb3da9ed0eb44e60d9ef1ee
BT_2S14_13 <email@email>
2024-11-08 09:59:33 -0500

    -First Commit

---
+++ src/DictionaryChecker.java
@@ -0,0 +1,8 @@
+
+public class DictionaryChecker {
+    public DictionaryChecker() {} // here to
+    public boolean isAWord (String Word)
+    {
+        return true; //placeholder value - implement word checker later
+    }
+}
---
+++ src/GameLogic.java
@@ -0,0 +1,7 @@
+public class GameLogic {
+
+    int Score = 0;
+    DictionaryChecker d = new DictionaryChecker();
+    public GameLogic (){}
+
+}
