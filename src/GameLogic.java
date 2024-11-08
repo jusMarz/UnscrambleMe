8c4fda820fa58c918eacb409179184261f5f3211
BT_2S14_13 <email@email>
2024-11-08 10:51:39 -0500

    - The real first commit - Created GameLogic, DictionaryChecker, and GameRunner - Began implementation of Gamelogic Methods fromm the Design Document

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -2,6 +2,51 @@ public class GameLogic {
 
     int Score = 0;
     DictionaryChecker d = new DictionaryChecker();
-    public GameLogic (){}
-//why wont this work
+    public GameLogic ()
+    {
+        Score = 0;
+    }
+
+    public int GetScore()
+    {
+        return Score;
+    }
+    public int WordScore (double rarityMultiplier, double lengthMultiplier, String word)
+    {
+        String letter;
+        int wordScore = 0;
+        for (int i = 0; i < word.length();i ++)
+        {
+            letter = (word.substring(i, i+1));
+            letter = letter.toUpperCase();
+            wordScore += ("ETAONRISHDLFCMUGYPWBVKJXZQ".indexOf(letter) ) * rarityMultiplier;
+        }
+        return wordScore +  (wordScore * (int) (word.length() * lengthMultiplier));
+    }
+
+    public boolean PossibleWord(String thisWord, String inThisWordQuestionMark)
+    {
+        boolean untilFalse;
+        untilFalse = d.isAWord(thisWord);
+        for (int i = 0; (i < thisWord.length()) && (untilFalse);i ++)
+        {
+            untilFalse = (inThisWordQuestionMark.contains(thisWord));
+
+        }
+        return untilFalse;
+    }
+
+    public String wordJumble (int length, int letterFrequency)
+    {
+        double allowedDegeneracy = 0;
+        int whereAreYou = 0;
+        String imInTheSoupStore = "";
+        for (int i = 0; i <= length;i ++)
+        {
+            whereAreYou = Math.random() * 26;
+
+            imInTheSoupStore += "ETAONRISHDLFCMUGYPWBVKJXZQ".charAt(whereAreYou);
+        }
+        return imInTheSoupStore;
+    }
 }
--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,7 +1,9 @@
 public class GameRunner {
     public static void main(String[] args) {
 
-
+        GameLogic g = new GameLogic();
+        System.out.println(g.WordScore(1, .2,"Gyat"));
+        System.out.println(g.PossibleWord("hui","rfewuihfiwhfuihwuifhuihweiuhw"));
 
 
 
