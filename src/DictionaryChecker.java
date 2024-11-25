028c258331e57f9d1d27a3d7a0f2a458baacf52c
BT_2S14_13 <email@email>
2024-11-25 10:57:39 -0500

    - Added comments to methods in DictionaryChecker and GameLogic for clarification even though the method names spell it out :skull: - Not sure if I already mentioned this but added an option for player to end the game early

--- src/DictionaryChecker.java
+++ src/DictionaryChecker.java
@@ -1,14 +1,27 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+/**
+ * The DictionaryChecker represents a dictionary reader. A dictionary reader can read the dictionary and search for words.
+ * Should've called this a WordSearcher
+ */
 
 public class DictionaryChecker {
     boolean wordValidity;
 
+    /**
+     * Constructor for the DictionaryChecker class. This creates a new instance of a Person given
+     * no parameters, because anyone* can check a dictionary.
+     */
     public DictionaryChecker() throws IOException
     {
         wordValidity = false;
     }
+
+    /**
+     * The isAWord method will determine the presence of a word in the dictionary.txt, given a word
+     * @param thisWord a String representing a potential word
+     */
 
     public boolean isAWord(String thisWord) throws IOException
     {
--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+/**
+ * The GameLogic class represents a game of Unscramble Me. A game consists of inputted words, a total score, a string of letters, and a time limit.
+ */
 
 public class GameLogic {
 
@@ -9,6 +12,14 @@ public class GameLogic {
     String gameWordJumble;
     DictionaryChecker d = new DictionaryChecker();
 
+    /**
+     * Constructor for the GameLogic class. This creates a new instance of a game given
+     * the below parameters.
+     *
+     * @param wordLength represents many letters will be given to the player.
+     * @param timeLength represents the time limit for the player
+     */
+
     public GameLogic (int wordLength, int timeLength) throws IOException
     {
         gameScore = 0;
@@ -16,47 +27,69 @@ public class GameLogic {
         gameTimeLength = timeLength;
         gameWordJumble = " ";
     }
-
+    /**
+     * This method will return all the inputs the user has entered.
+    */
     public String GetInputtedWords ()
     {
         return userWordInputs;
     }
-
+    /**
+     * This method will return the total amassed score the user has earned.
+     */
     public int GetScore()
     {
         return gameScore;
     }
-
+    /**
+     * This method will return the number of letters the user was given to work with.
+     */
     public int GetWordLength()
     {
         return gameWordLength;
     }
-
+    /**
+     * This method will return the time limit given to the user.
+     */
     public int GetTimeLength()
     {
         return gameTimeLength;
     }
-
+    /**
+     * This method will return the letters given to the player.
+     */
     public String GetWordJumble()
     {
         return gameWordJumble;
     }
-
+    /**
+     * This method will increase the gameScore.
+     * @param scoreUp represents how much the score will increase.
+     */
     public void IncreaseScore(int scoreUp)
     {
         gameScore += scoreUp;
     }
-
+    /**
+     * This method will set the letters the user will receive.
+     * @param jumbleSet represents the string of letters the player gets to use.
+     */
     public void setGameWordJumble(String jumbleSet)
     {
         gameWordJumble = jumbleSet;
     }
-
+    /**
+     * This method will add a word to a string of already entered words.
+     * @param word represents the user's entered word.
+     */
     public void addInputtedWord(String word)
     {
         userWordInputs += word.toUpperCase() + " ";
     }
-
+    /**
+     * This method will determine the score of a certain word.
+     * @param word represents the word to be scored.
+     */
     public int wordScore(String word)
     {   String letter;
         int wordScore = 0;
